6ae82a30b4835454796638060310c430e4de13e5
shafali03 <shaf03sm@gmail.com>
2020-07-03 09:08:26 +0100

    Initial Setup

---
+++ server.js
@@ -0,0 +1,33 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./controllers/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+})
+  .then(() => console.log('DB Connected!'))
+  .catch(err => {
+    console.log(`DB Connection Error: ${err.message}`);
+  });
+
+
+
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)

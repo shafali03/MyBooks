d76d5bbc9a50b4da06f8df91b2392b280409029a
shafali03 <shaf03sm@gmail.com>
2020-07-03 19:36:54 +0100

    feat: Can create new author and redirect to author page

---
+++ controllers/authors.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+// All author route
+router.get('/', (req, res) => {
+  res.render('authors/index')
+})
+
+// New Author Route
+router.get('/new', (req, res) => {
+  res.render('authors/new', { author: new Author() })
+})
+
+// Create author Route
+router.post('/', (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  })
+  author.save((err, newAuthor) => {
+    if (err) {
+      res.render('authors/new', {
+        author: author,
+        errorMessage: 'Error creating Author'
+      })
+    } else {
+      res.redirect(`authors`)
+    }
+  })
+})
+
+module.exports = router
